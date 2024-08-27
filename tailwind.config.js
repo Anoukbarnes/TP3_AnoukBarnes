8809e94666c3c0632d49dc908e69a0795618a62f
Anoukbarnes <anouk721@gmail.com>
2024-08-27 13:34:17 -0400

    Éléments DOM

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+const form = document.getElementById('form');
+const firstname = document.getElementById('first-name'); 
+const lastname = document.getElementById('last-name');
+const email = document.getElementById('email');
+const tel = document.getElementById('tel');
+const entreprise= document.getElementById('entreprise');
+const delais = document.getElementById('delais');
+const budget = document.getElementById('budget');
+const description = document.getElementById('description');
+
