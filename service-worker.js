01cfdfbb2f74b97e08fc1acab158d797aaf368af
Anoukbarnes <anouk721@gmail.com>
2024-09-09 22:45:59 -0400

    Ajustement erreur

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-//Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v19';
-//Add list of files to cache here.
+// Update cache names any time any of the cached files change.
+const CACHE_NAME = 'static-cache-v20';
+// Add list of files to cache here.
 const FILES_TO_CACHE = [
     'offline.html',
     'index.html',
@@ -12,9 +12,9 @@ const FILES_TO_CACHE = [
     'renova.html'
 ];
 
-//INSTALLATION SERVICE WORKER
+// INSTALLATION SERVICE WORKER
 self.addEventListener('install', (evt) => {
-     console.log('[ServiceWorker] Install');
+    console.log('[ServiceWorker] Install');
     // Precache static resources here.
     evt.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
@@ -26,41 +26,39 @@ self.addEventListener('install', (evt) => {
         })
     );
     self.skipWaiting();
-    
 });
 
-//ACTIVATION
+// ACTIVATION
 self.addEventListener('activate', (evt) => {
     console.log('[ServiceWorker] Activate');
-    //Remove previous cached data from disk.
+    // Remove previous cached data from disk.
     evt.waitUntil(
         caches.keys().then((keyList) => {
             return Promise.all(keyList.map((key) => {
                 if (key !== CACHE_NAME) {
                     console.log('[ServiceWorker] Removing old cache', key);
                     return caches.delete(key);
-               }
+                }
             }));
         })
-    )
-    self.clients.claim(); 
+    );
+    self.clients.claim();
 });
 
-//ACCÈS À UNE RESSOURCE
+// ACCÈS À UNE RESSOURCE
 self.addEventListener('fetch', (evt) => {
-     console.log('[ServiceWorker] Fetch', evt.request.url);
-    //Add fetch event handler here.
+    console.log('[ServiceWorker] Fetch', evt.request.url);
+    // Add fetch event handler here.
     if (evt.request.mode !== 'navigate') {
-    // Not a page navigation, bail.
-    return;
-    } 
+        // Not a page navigation, bail.
+        return;
+    }
     evt.respondWith(
-        fetch(evt.request)
-        .catch(() => {
-            return caches.open(CACHE_NAME) 
+        fetch(evt.request).catch(() => {
+            return caches.open(CACHE_NAME)
                 .then((cache) => {
                     return cache.match('offline.html');
-     });
-    })
+                });
+        })
     );
 });
